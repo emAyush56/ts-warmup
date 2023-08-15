ca3f49de0af19d08282541e9015b68fe8da5cb3d
Ayush Saha <searchme.ayush@gmail.com>
2023-08-15 21:31:34 +0530

    Fixed line number in a comment in types.ts file

--- 3_primitives/types.ts
+++ 3_primitives/types.ts
@@ -24,7 +24,7 @@ const char: string = "2";
 const strCat: string = num3 + char;
 console.log(strCat);
 
-// Why does line no. 3 doesnot give any type error? As I am trying to add a number to a string
+// Why does line no. 24 doesnot give any type error? As I am trying to add a number to a string
 // In JavaScript/TypeScript, when you add a number to a string, the number is automatically converted to a string and then concatenated with the other string. This is called type coercion.
 // Type conversion is similar to type coercion because they both convert values from one data type to another with one key difference — type coercion is implicit whereas type conversion can be either implicit or explicit.
 // In your code example, num3 is of type number, and char is of type string. When you try to concatenate them using the + operator, TypeScript automatically converts the number to a string and then concatenates it with the other string.
